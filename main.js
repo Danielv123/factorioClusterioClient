c6d954461c18342025e350363f8b2753bf47e254
Danielv123 <tranen.bananen@gmail.com>
2017-04-02 17:04:10 +0200

    Display instanceName, reorganize

--- main.js
+++ main.js
@@ -8,84 +8,90 @@ const electron = require("electron");
 const remote = require("electron").remote;
 const mainProcess = remote.require("./index");
 
+const needle = require("needle");
+
+var globalData;
 setInterval(function(){
 	// IPC send sends text/json/whatever to index.js or anything on the nodeside that cares to listen
 	ipc.send('getServers', "plz send");
-}, 500)
-var globalData;
-ipc.on("setServers", function(event, data){
-	//console.log(data)
-	globalData = data;
-	let html = "";
-	
-	// generate title row
-	let keys = {
-		"Slave ID":"unique",
-		"IP":"publicIP",
-		"mods":"mods",
-		"Status":"time",
-		"Players":"playerCount",
-	}
-	
-	// start with printing keys
-	html += "<tr>"
-	for (let key in keys) {
-		html += "<td>"+key+"</td>";
-	}
-	html += "</tr>"
-	
-	// keep going with printing slaves corresponding to the keys
-	for(let slave in data){
-		// only print servers that has been online some time the last 60 seconds (in ms)
-		if(Date.now() - data[slave].time < 120000) {
+	needle.get(config.get("masterAddress")+'/api/slaves', function(error, response, data) {
+		if (!error) {
+			//console.log(data)
+			globalData = data;
+			let html = "";
+			
+			// generate title row
+			let keys = {
+				"Name":"instanceName",
+				"Slave ID":"unique",
+				"IP":"publicIP",
+				"mods":"mods",
+				"Status":"time",
+				"Players":"playerCount",
+			}
+			
+			// start with printing keys
 			html += "<tr>"
 			for (let key in keys) {
-				let currentKey = keys[key]
-				if(currentKey == "time") {
-					// print time in seconds instead of unix time
-					let seconds = Math.floor((Date.now()-data[slave][currentKey])/1000)
-					
-					// if slave pinged last 15 seconds, display it as online
-					// otherwise alert us to the fact that it is missing.
-					if (seconds < 15) {
-						html += "<td style='min-width:110px;'>Online</td>";
-					} else {
-						html += "<td style='min-width:110px;'>seen "+seconds+"s ago</td>";
+				html += "<td>"+key+"</td>";
+			}
+			html += "</tr>";
+			
+			// keep going with printing slaves corresponding to the keys
+			for(let slave in data){
+				// only print servers that has been online some time the last 60 seconds (in ms)
+				console.log(data[slave]);
+				if(Date.now() - data[slave].time < 120000) {
+					html += "<tr>"
+					for (let key in keys) {
+						let currentKey = keys[key]
+						if(currentKey == "time") {
+							// print time in seconds instead of unix time
+							let seconds = Math.floor((Date.now()-data[slave][currentKey])/1000)
+							
+							// if slave pinged last 15 seconds, display it as online
+							// otherwise alert us to the fact that it is missing.
+							if (seconds < 15) {
+								html += "<td style='min-width:110px;'>Online</td>";
+							} else {
+								html += "<td style='min-width:110px;'>seen "+seconds+"s ago</td>";
+							}
+						} else if (currentKey == "mods") {
+							// Make modlist look nice
+							html += "<td>"
+							for(let i = 0;i<data[slave].mods.length;i++){
+								// print one mod per line
+								html += data[slave].mods[i].modName + "\n"
+							}
+							html += "</td>"
+						} else if (currentKey == "publicIP") {
+							html += "<td>"+data[slave][currentKey] + ":" + data[slave].serverPort
+						} else {
+							if (data[slave][keys[key]]){
+								html += "<td>"+data[slave][keys[key]]+"</td>"
+							} else {
+								html += "<td>Unsupported</td>"
+							}
+						}
 					}
-				} else if (currentKey == "mods") {
-					// Make modlist look nice
-					html += "<td>"
-					for(let i = 0;i<data[slave].mods.length;i++){
-						// print one mod per line
-						html += data[slave].mods[i].modName + "\n"
-					}
-					html += "</td>"
-				} else if (currentKey == "publicIP") {
-					html += "<td>"+data[slave][currentKey] + ":" + data[slave].serverPort
-				} else {
-					if (data[slave][keys[key]]){
-						html += "<td>"+data[slave][keys[key]]+"</td>"
-					} else {
-						html += "<td>Unsupported</td>"
-					}
+					html += '<td id="temp" onclick="launchFactorio(this)"><button class="button launchButton">Join server</button></td>'
+					html += "</tr>"
 				}
 			}
-			html += '<td id="temp" onclick="launchFactorio(this)"><button class="button launchButton">Join server</button></td>'
-			html += "</tr>"
+			
+			// turn our HTML text into dom elements for comparison
+			var div = document.createElement('div');
+			div.innerHTML = html;
+			var div2 = document.createElement('div');
+			div2.innerHTML = document.querySelector("#slaves").innerHTML;
+			
+			// use special browser function for comparing dom elements
+			if(!div.isEqualNode(div2)) {
+				document.querySelector("#slaves").innerHTML = html
+			}
 		}
-	}
-	
-	// turn our HTML text into dom elements for comparison
-	var div = document.createElement('div');
-	div.innerHTML = html;
-	var div2 = document.createElement('div');
-	div2.innerHTML = document.querySelector("#slaves").innerHTML;
-	
-	// use special browser function for comparing dom elements
-	if(!div.isEqualNode(div2)) {
-		document.querySelector("#slaves").innerHTML = html
-	}
-});
+	});
+}, 500)
 
 // tell node to launch factorio
 // this is some shitty patchwork solution that is 100% guaranteed to break
